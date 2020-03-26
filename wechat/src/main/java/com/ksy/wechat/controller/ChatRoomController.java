7a4dc5314ac92d855140d635d7731a7ce810d095
kimsangyoun <ksang91@naver.com>
2020-03-26 19:28:58 +0900

    채팅 클라이언트 수정.

--- wechat/src/main/java/com/ksy/wechat/controller/ChatRoomController.java
+++ wechat/src/main/java/com/ksy/wechat/controller/ChatRoomController.java
@@ -15,8 +15,10 @@ import org.springframework.web.client.RestTemplate;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ksy.wechat.dto.ChatRoomDto;
 import com.ksy.wechat.dto.SingleResultDto;
 import com.ksy.wechat.utill.CookieUtill;
+import com.sun.org.apache.xml.internal.utils.URI.MalformedURIException;
 
 import java.util.Collections;
 import java.util.List;
@@ -26,7 +28,6 @@ import javax.servlet.http.HttpServletRequest;
 
 
 @Controller
-@RequestMapping("/chat")
 public class ChatRoomController {
 	@Autowired
 	RestTemplate restTemplate;
@@ -41,7 +42,7 @@ public class ChatRoomController {
     public String roomDetail(Model model, @PathVariable String roomId,HttpServletRequest request) throws JsonMappingException, JsonProcessingException {
     	
     	final String uri = "http://localhost:8080/v1/auth/me";
-
+    	System.out.println(roomId+"À½ ·ë");
         String token = CookieUtill.getValue(request, "accesstoken");
         HttpHeaders headers = new HttpHeaders();
         headers.set("X-AUTH-TOKEN", token);
@@ -72,9 +73,63 @@ public class ChatRoomController {
             System.out.println("Request Failed");
             System.out.println(response.getStatusCode());
         }
-    	
+        model.addAttribute("accesstoken", token);
         model.addAttribute("roomId", roomId);
         return "/roomdetail";
+    }
+    @PostMapping("/room/makeroom")
+    @ResponseBody
+    public String MakeRoom(@RequestBody ChatRoomDto room, HttpServletRequest request) throws MalformedURIException {
+
+        final String uri = "http://localhost:8080/v1/room";
+
+        String token = CookieUtill.getValue(request, "accesstoken");
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("X-AUTH-TOKEN", token);
+        // set `content-type` header
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        // set `accept` header
+        headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+
+        // build the request
+        HttpEntity<ChatRoomDto> entity = new HttpEntity<>(room, headers);
+        ResponseEntity<String> response = restTemplate.postForEntity(uri, entity, String.class);
+        return response.getBody().toString();
+    }
+
+    @GetMapping("/room/readroom")
+    @ResponseBody
+    public String ReadRoom(HttpServletRequest request) throws MalformedURIException {
+
+        final String uri = "http://localhost:8080/v1/rooms";
+
+        String token = CookieUtill.getValue(request, "accesstoken");
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("X-AUTH-TOKEN", token);
+        // set `content-type` header
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        // set `accept` header
+        headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+        // build the request
+        HttpEntity entity = new HttpEntity(headers);
+
+        ResponseEntity<String> response = restTemplate.exchange(
+                uri,
+                HttpMethod.GET,
+                entity,
+                String.class,
+                1
+        );
+        // check response
+        if (response.getStatusCode() == HttpStatus.OK) {
+            System.out.println("Request Successful.");
+            System.out.println(response.getBody());
+
+        } else {
+            System.out.println("Request Failed");
+            System.out.println(response.getStatusCode());
+        }
+        return response.getBody();
     }
 
 }
--- wechat/src/main/java/com/ksy/wechat/controller/MainController.java
+++ wechat/src/main/java/com/ksy/wechat/controller/MainController.java
@@ -30,63 +30,6 @@ public class MainController {
         // View attribute
         return "main";
     }
-    @GetMapping("/roomdetail")
-    public String MainViewDettail(Model model) {
-        // View attribute
-        return "roomdetail";
-    }
-    @PostMapping("/makeroom")
-    @ResponseBody
-    public String MakeRoom(@RequestBody ChatRoomDto room, HttpServletRequest request) throws MalformedURIException {
+   
 
-        final String uri = "http://localhost:8080/v1/room";
-
-        String token = CookieUtill.getValue(request, "accesstoken");
-        HttpHeaders headers = new HttpHeaders();
-        headers.set("X-AUTH-TOKEN", token);
-        // set `content-type` header
-        headers.setContentType(MediaType.APPLICATION_JSON);
-        // set `accept` header
-        headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
-
-        // build the request
-        HttpEntity<ChatRoomDto> entity = new HttpEntity<>(room, headers);
-        ResponseEntity<String> response = restTemplate.postForEntity(uri, entity, String.class);
-        return response.getBody().toString();
-    }
-
-    @GetMapping("/readroom")
-    @ResponseBody
-    public String ReadRoom(HttpServletRequest request) throws MalformedURIException {
-
-        final String uri = "http://localhost:8080/v1/rooms";
-
-        String token = CookieUtill.getValue(request, "accesstoken");
-        HttpHeaders headers = new HttpHeaders();
-        headers.set("X-AUTH-TOKEN", token);
-        // set `content-type` header
-        headers.setContentType(MediaType.APPLICATION_JSON);
-        // set `accept` header
-        headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
-        // build the request
-        HttpEntity entity = new HttpEntity(headers);
-
-        ResponseEntity<String> response = restTemplate.exchange(
-                uri,
-                HttpMethod.GET,
-                entity,
-                String.class,
-                1
-        );
-        // check response
-        if (response.getStatusCode() == HttpStatus.OK) {
-            System.out.println("Request Successful.");
-            System.out.println(response.getBody());
-
-        } else {
-            System.out.println("Request Failed");
-            System.out.println(response.getStatusCode());
-        }
-        return response.getBody();
-    }
 }
